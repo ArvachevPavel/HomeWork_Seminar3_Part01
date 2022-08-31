13e1afb9ae661dbf9c77252e8f2a9a6dcbd3f8aa
Pavel Arvachev <arvachev@gmail.com>
2022-08-31 12:29:09 +0300

    add Tassk 27

---
+++ Task027/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+Console.WriteLine("Введите число ");
+int a = int.Parse(Console.ReadLine());
+int count = 0;
+while (a != 0)
+{
+    int LastDig = a%10;
+    count = count + LastDig;
+    a = a/10;
+
+}
+Console.WriteLine("Сумма цифр в этом числе равна " + count);
+
+
